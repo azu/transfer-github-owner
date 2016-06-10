67c9d26382f121b744e7db4e1b150fb974e7094b
azu <azuciao@gmail.com>
2016-06-11 01:16:44 +0900

    update

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,6 +3,6 @@ var rewrite = require('../');
 var args = require("args");
 args.option('from', "from owner name")
     .option('to', "to owner name");
-var argv = args.parse(process.argv)
+var argv = args.parse(process.argv);
 var result = rewrite(process.cwd(), argv);
 result && console.log(result);
